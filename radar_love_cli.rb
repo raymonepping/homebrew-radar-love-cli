7d3b0a04418747e1b77747e360d15dd1e05e8a3c
Raymon Epping <your@email.com>
2025-07-06 20:50:29 +0200

    06/07/2025 - Updated configuration and fixed bugs

--- radar_love_cli.rb
+++ radar_love_cli.rb
@@ -1,8 +1,9 @@
 class RadarLoveCli < Formula
   desc "CLI toolkit for simulating secret leaks and triggering GitHub PR scans"
-  homepage "https://github.com/raymonepping/radar_love_cli"
-  url      "https://github.com/raymonepping/radar_love_cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256   "9731be25ab825ed6ddfc97ef5601dc4add17462714bddcfbf71b71acd6a6e3e1"
+  homepage  "https://github.com/raymonepping/homebrew-radar_love_cli"
+  url       "https://github.com/raymonepping/homebrew-radar_love_cli/archive/refs/tags/v1.0.0.tar.gz"
+  sha256    "d8623900b69ddaba074b1c4781ca6ad3764319a9826e245882abe5883cf275bd"
+
   license "MIT"
   version  "1.0.0"
 
