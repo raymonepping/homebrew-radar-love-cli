ddcc94cbb42d892f3b50ea18076b050850350f88
Raymon Epping <your@email.com>
2025-07-06 22:31:44 +0200

    chore(formula): bump to v1.2.3

--- radar_love_cli.rb
+++ radar_love_cli.rb
@@ -2,7 +2,7 @@ class RadarLoveCli < Formula
   desc "CLI toolkit for simulating secret leaks and triggering GitHub PR scans"
   homepage "https://github.com/raymonepping/radar_love_cli"
   url "https://github.com/raymonepping/radar_love_cli/archive/refs/tags/v1.2.3.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "974220933135c75af84b623dc9ef19652596607f872b4773702e3c2e84153fd0"
   license "MIT"
   version "1.2.3"
 
