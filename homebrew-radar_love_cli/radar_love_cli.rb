f2e78a09241d92aa3097ca5a0393c01604795cf1
Raymon Epping <your@email.com>
2025-07-07 09:49:57 +0200

    chore(formula): update to v1.4.0

---
+++ homebrew-radar_love_cli/radar_love_cli.rb
@@ -0,0 +1,33 @@
+class RadarLoveCli < Formula
+  desc "CLI toolkit for simulating secret leaks and triggering GitHub PR scans"
+  homepage "https://github.com/raymonepping/radar_love_cli"
+  url "https://github.com/raymonepping/homebrew-radar_love_cli/archive/refs/tags/v1.4.0.tar.gz"
+  sha256 "d720f3c5d373596b32ece280f6d8a9f66a4485b1db02fae81a7699289b598d5a"
+  license "MIT"
+  version "1.4.0"
+
+  depends_on "bash"
+
+  def install
+    bin.install "bin/radar_love" => "radar_love"
+    pkgshare.install "core", "templates"
+    doc.install "README.md"
+  end
+
+  def caveats
+    <<~EOS
+      To get started, run:
+        radar_love --help
+
+      The core scripts and templates are located in:
+        #{opt_pkgshare}
+
+      If you need to reference them directly in your scripts, use:
+        export RADAR_LOVE_HOME=#{opt_pkgshare}
+    EOS
+  end
+
+  test do
+    assert_match "radar_love", shell_output("#{bin}/radar_love --version")
+  end
+end
