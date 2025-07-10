3fbef38700b81bc3fd2fd5be12df5f3a2a522508
Raymon Epping <your@email.com>
2025-07-10 17:58:24 +0200

    chore(formula): update to v2.8.4

--- Formula/radar-love-cli.rb
+++ Formula/radar-love-cli.rb
@@ -1,10 +1,10 @@
 class RadarLoveCli < Formula
   desc "CLI toolkit for simulating secret leaks and triggering GitHub PR scans"
   homepage "https://github.com/raymonepping/radar_love_cli"
-  url "https://github.com/raymonepping/homebrew-radar-love-cli/archive/refs/tags/v2.8.3.tar.gz"
-  sha256 "deafef98f607583eaf66fc81d7c4340710c2e049f9f28bf1901bee630831ead8"
+  url "https://github.com/raymonepping/homebrew-radar-love-cli/archive/refs/tags/v2.8.4.tar.gz"
+  sha256 "a17f2b73d6dcb8d859095fd8fe2638a9e41512a17548c4485139acd42bdd793b"
   license "MIT"
-  version "2.8.3"
+  version "2.8.4"
 
   depends_on "bash"
 
